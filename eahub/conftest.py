405f824604832c06e2d8ca76e9fcaa854053e213
Taymon A. Beal <taymon@google.com>
2019-06-11 23:30:15 -0400

    Block WebDriver fixture on node availability

--- eahub/conftest.py
+++ eahub/conftest.py
@@ -7,5 +7,14 @@ env = environ.Env()
 
 @pytest.fixture(scope="session", params=env.list("BROWSERS"))
 def session_capabilities(request, session_capabilities):
+    import time, requests
+    for _ in range(60):
+        json = requests.get("http://webdriver:4444/grid/api/hub?configuration=nodes").json()
+        browsers = {browser["browser"] for node in json["nodes"] for browser in node["browsers"]}
+        if request.param in browsers:
+            break
+        time.sleep(0.5)
+    else:
+        raise IOError("could not connect to browser")
     session_capabilities["browserName"] = request.param
     return session_capabilities
